0788dfe5fdeeef447270dc6d2716de3fb7ce61e5
Nick <nick.brown@ed.ac.uk>
2016-04-13 20:33:51 +0100

    Updates to localheap to work for multiple processes

--- src/localheap.c
+++ src/localheap.c
@@ -19,42 +19,53 @@
 #include <string.h>
 #include <math.h>
 
-static int myRank, totalRanks;
 void *baseLocalHeapAddress;
 
 memkind_t LOCALHEAP_KIND;
 static void *my_pmem_mmap(struct memkind *, void *, size_t);
 
-// needs to be modified for multiple processes
-
-void *initialise_local_heap_space(int myRanka, int totalRanksa, void *global_base_address) {
+void *initialise_local_heap_space(int myRank, int totalRanks, void *global_base_address) {
+  int i;
   struct memkind_ops *my_memkind_ops = (struct memkind_ops *)memkind_malloc(MEMKIND_DEFAULT, sizeof(struct memkind_ops));
   memcpy(my_memkind_ops, &MEMKIND_PMEM_OPS, sizeof(struct memkind_ops));
   my_memkind_ops->mmap = my_pmem_mmap;
 
-  myRank = myRanka;
-  totalRanks = totalRanksa;
-  struct distmem_ops LOCALHEAP_MEMORY_VTABLE = {
-      .dist_malloc = NULL, .dist_create = distmem_arena_create, .memkind_operations = my_memkind_ops};
-  distmem_create(&LOCALHEAP_MEMORY_VTABLE, "localheap", &LOCALHEAP_KIND);
+  struct distmem_ops *localheap_vtable = (struct distmem_ops *)memkind_malloc(MEMKIND_DEFAULT, sizeof(struct distmem_ops));
+  localheap_vtable->dist_malloc = NULL;
+  localheap_vtable->dist_create = distmem_arena_create;
+  localheap_vtable->memkind_operations = my_memkind_ops;
+  localheap_vtable->dist_determine_distribution = NULL;
 
-  size_t jemk_chunksize_exponent;
-  size_t s = sizeof(jemk_chunksize_exponent);
-  jemk_mallctl("opt.lg_chunk", &jemk_chunksize_exponent, &s, NULL, 0);
+  distmem_create(localheap_vtable, "localheap", &LOCALHEAP_KIND);
 
-  int jemk_chunksize = (int)pow(2.0, jemk_chunksize_exponent);
+  unsigned long start_addresses[totalRanks];
+  if (myRank == 0) {
+    size_t jemk_chunksize_exponent;
+    size_t s = sizeof(jemk_chunksize_exponent);
+    jemk_mallctl("opt.lg_chunk", &jemk_chunksize_exponent, &s, NULL, 0);
+
+    int jemk_chunksize = (int)pow(2.0, jemk_chunksize_exponent);
+    for (i = 0; i < totalRanks; i++) {
+      if (i == 0) {
+        start_addresses[i] = (unsigned long)global_base_address;
+      } else {
+        start_addresses[i] = start_addresses[i - 1] + LOCAL_HEAP_SIZE;
+      }
+      start_addresses[i] = roundup(start_addresses[i], jemk_chunksize);
+    }
+  }
+  MPI_Bcast(start_addresses, totalRanks, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
 
   struct memkind_pmem *priv = LOCALHEAP_KIND->priv;
   priv->fd = 0;
-  priv->addr = (void *)roundup((uintptr_t)global_base_address, jemk_chunksize);
+  priv->addr = (void *)start_addresses[myRank];
   priv->max_size = LOCAL_HEAP_SIZE;
   priv->offset = 0;
 
   mlock(priv->addr, priv->max_size);
 
-  int i;
   for (i = 0; i < totalRanks; i++) {
-    registerMemory(priv->addr + (i * LOCAL_HEAP_SIZE), LOCAL_HEAP_SIZE, i);
+    registerMemory((void *)start_addresses[i], LOCAL_HEAP_SIZE, i);
   }
   MPI_Win win;
   MPI_Win_create(priv->addr, LOCAL_HEAP_SIZE, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
