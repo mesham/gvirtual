102bdb2b395704b6847e6a312bee8b045deece4f
Nick <nick.brown@ed.ac.uk>
2016-04-15 18:26:42 +0100

    Better explanation of example

--- example/rmaops.c
+++ example/rmaops.c
@@ -27,18 +27,22 @@ int main(int argc, char* argv[]) {
   MPI_Barrier(MPI_COMM_WORLD);  // To ensure the values are set before we copy into the cache
   if (myrank == 0) {
     unsigned long intAddress;
+    // Recv the global virtual address of rank 1's local heap allocated int array
     MPI_Recv(&intAddress, 1, MPI_UNSIGNED_LONG, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // Grab the remote data based upon its global virtual address space and return the location in the cache (part of my local heap)
     int* cachedRemoteData = (int*)gv_acquireMutable((void*)intAddress, sizeof(int) * 10);
     for (i = 0; i < 10; i++) {
-      cachedRemoteData[i] *= 10;
+      cachedRemoteData[i] *= 10;  // Modify the cached value
     }
-    gv_commitMakeConst((void*)intAddress);
-    MPI_Barrier(MPI_COMM_WORLD);  // Ensure the updated values are committed
-  } else {
+    gv_commitMakeConst((void*)intAddress);  // Commit the cached value back to original location on rank 1
+    MPI_Barrier(MPI_COMM_WORLD);            // Ensure the updated values are committed before rank 1 does the display
+  } else if (myrank == 1) {
     unsigned long intAddress = (unsigned long)data;
-    MPI_Send(&intAddress, 1, MPI_UNSIGNED_LONG, 0, 0, MPI_COMM_WORLD);
-    MPI_Barrier(MPI_COMM_WORLD);  // Ensure the updated values are committed
-    printf("%d\n", data[0]);
+    MPI_Send(&intAddress, 1, MPI_UNSIGNED_LONG, 0, 0, MPI_COMM_WORLD);  // Send the global virtual address to rank 0
+    MPI_Barrier(MPI_COMM_WORLD);  // Ensure the updated values are committed before I display the value
+    printf("%d\n", data[0]);      // This will display the value updated by rank 0
+  } else {
+    MPI_Barrier(MPI_COMM_WORLD);
   }
 
   memkind_free(LOCALHEAP_KIND, data);
